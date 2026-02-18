bf84ace9fd8f8fd95885fe8731860867c04a2fc4
zaferavcilari-svg <zaferavcilari@gmail.com>
2026-02-18 22:27:50 +0300

    Delete chat.js

--- chat.js
+++
@@ -1,9 +0,0 @@
-const chatBtn = document.getElementById("chatBtn");
-const chatPanel = document.getElementById("chatPanel");
-
-chatBtn.onclick = () => {
-  chatPanel.style.display =
-    chatPanel.style.display === "block" ? "none" : "block";
-};
-
-chatPanel.innerHTML = "<h3>Sohbet</h3><p>Demo sürüm</p>";
