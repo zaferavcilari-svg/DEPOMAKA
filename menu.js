17590b30ac2d0fea7b94b231faffc91a30433175
zaferavcilari-svg <zaferavcilari@gmail.com>
2026-02-18 22:57:09 +0300

    menu.js 0.1

---
+++ menu.js
@@ -0,0 +1,11 @@
+function goToGame() {
+  window.location.href = "game.html"; 
+}
+
+function goToSettings() {
+  window.location.href = "settings.html";
+}
+
+function goToProfile() {
+  window.location.href = "profile.html";
+}
